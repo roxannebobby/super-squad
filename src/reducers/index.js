42eea6a1f5775637fb9bd7fc9577b713a27950ad
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-03-16 00:25:36 -0500

    store setup completed

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import characters_data from '../data/characters_data.json';
+
+function characters(state = characters_data, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
+
+export default characters;
