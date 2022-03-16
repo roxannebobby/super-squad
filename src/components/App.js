0088bcc28dbba97d6172703b0c1c0ad0b0333d9d
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-03-16 01:42:50 -0500

    used redux bind to bind actions to props

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../components/App.css';
+import CharacterList from './CharacterList';
 
 export const App = () => {
 	return (
 		<div className='well well-large center'>
 			<h1>Super Squad Builder</h1>
+			<CharacterList />
 		</div>
 	);
 };
